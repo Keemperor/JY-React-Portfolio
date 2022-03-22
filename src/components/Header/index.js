7dfb9c2c7300eefa2ddd7c5252af1d79b7a6b77b
Keemperor <jacobotau@hotmail.com>
2022-03-21 23:25:04 -0400

    reverted and fixed issues

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,55 +1,22 @@
-import React, { useState } from 'react';
-import Nav from "../Nav";
-import About from "../About";
-import Contact from "../Contact";
-//import Portfolio from "./Portfolio";
-//import Resume from "./Resume";
+import React from "react";
+import Navigation from "../Nav";
 
-function Header() {
-  const [currentPage, handlePageChange] = useState("About");
+function Header(props) {
+	const { currentTab, setCurrentTab } = props;
 
-  // The renderPage method uses a switch statement to render the appropriate current page
-  const renderPage = () => {
-    switch (currentPage) {
-      case "About":
-        return <About />;
-     // case "Portfolio":
-       // return <Portfolio />;
-      case "Contact":
-        return <Contact />;
-     // case "Resume":
-       // return <Resume />;
-
-      default:
-        return <About />;
-    }
-  };
-
-  return (
-    <div>
-      <nav className="navbar">
-        <div className="navbar-brand">
-          <a
-            className="navbar-item"
-            rel="noreferrer"
-            target="_blank"
-            href="https://github.com/EmilyNecciai?tab=repositories"
-          >
-            <span className="content is-large">Emily Necciai Mayeski</span>
-          </a>
-        </div>
-      </nav>
-      {/* Pass the state value and the setter as props to NavTabs */}
-      <Nav
-        currentPage={currentPage}
-        handlePageChange={handlePageChange}
-      />
-      {/* Call the renderPage function passing in the currentPage */}
-      <main>
-        <div>{renderPage(currentPage)}</div>
-      </main>
-    </div>
-  );
+	return (
+		<header >
+			<div className="footer">
+				<h2 >Jacob Yarce React Portfolio</h2>
+			</div>
+			<div className="footer">
+				<Navigation 
+					currentTab={currentTab}
+					setCurrentTab={setCurrentTab}
+				></Navigation>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
